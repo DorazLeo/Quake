de7091cea7d068b9a3d8c94d858ea8f186828b8a
leofreddi.dorazio.develop@gmail.com <ClDl2019!>
2019-07-18 12:51:15 +0200

    Cretato Parser per visibilità palette e modificato pom per heroku

---
+++ src/main/java/it/quake/utilities/VisibilityConditionParser.java
@@ -0,0 +1,20 @@
+package it.quake.utilities;
+
+/**
+ * Class for parse visibility conditions of palette and palette values
+ *
+ * @author Christopher Leofreddi
+ */
+public class VisibilityConditionParser {
+
+    /**
+     *
+     * @param condition
+     * @return
+     */
+    public static boolean parseString(String condition){
+
+        return true;
+    }
+
+}
